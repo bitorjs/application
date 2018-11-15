f51dbf2a235912477548a7ea0d3ffbe68400f423
huangzj <huangzj@huangzj.mzftech.cn>
2018-11-15 15:00:56 +0800

    fix

--- Application.js
+++ Application.js
@@ -13,9 +13,6 @@ class Application extends EventEmitter {
     this.middleware = [];
     option = option || {}
     this.$route = new Router();
-    // this.$route.register('/a11', 'GET', {}, () => {})
-    // this.$route.register('/a12', 'GET', {}, () => {})
-    // this.$route.register("*", "GET", {}, () => {})
     this.req = new Request(this);
     this.ctx = new Context(this, this.req)
     this.mode = option.mode || "hash";
@@ -34,33 +31,12 @@ class Application extends EventEmitter {
       this.ctx.url = url;
       this.handle_request(this.ctx, fn)
     }
-    // () => {
-
-    //   let hash = window.location.hash;
-    //   if (hash.charAt(0) === '#') hash = hash.slice(1);
-    //   if (hash.charAt(0) !== '/') hash = `/${hash}`;
-
-    //   let routes = this.$route.match(hash, 'GET');
-    //   routes.forEach(route=>{
-    //     // app.action(route.)
-    //     route.handle((res, res)=>{
-
-    //     })
-    //   })
-    //   console.log(routes)
-    // }
   }
 
   handle_request(ctx, fnMiddleware) {
     return fnMiddleware(ctx).then(() => {
-      console.log('hand request')
+      console.log('request end')
     });
-  }
-
-  initEvents() {
-    this.on('message', (a) => {
-      console.log('message:', a)
-    })
   }
 
   use(fn) {
